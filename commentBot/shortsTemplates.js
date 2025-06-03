6c1f3d773752f0d48a7466f98575c574d7f7e8ff
DanielMadden <danielfpjobs@gmail.com>
2025-06-03 13:08:28 -0600

    icons, not seeds

--- commentBot/shortsTemplates.js
+++ commentBot/shortsTemplates.js
@@ -1,10 +1,21 @@
 module.exports = (seed) => {
   let icons = ["👌", "📈", "🔥", "🤑", "💰", "👀"];
-  let icon = icons[Math.floor(Math.random() * seeds.length)]
-  let slangsGood = ["Noice","Dope","Litty","To the moon", "LFG", "Lit", "Fire", "Based", "Send it", "This is the way"]
-  let slangGood = slangsGood[Math.floor(Math.random() * slangsGood.length)]
-  let slangGoodLowercase = slangGood.toLowerCase()
-  let slangsBad = ["Bruh", "SMH", "frfr", "Aint no way", "This ain't it"]
+  let icon = icons[Math.floor(Math.random() * icons.length)];
+  let slangsGood = [
+    "Noice",
+    "Dope",
+    "Litty",
+    "To the moon",
+    "LFG",
+    "Lit",
+    "Fire",
+    "Based",
+    "Send it",
+    "This is the way",
+  ];
+  let slangGood = slangsGood[Math.floor(Math.random() * slangsGood.length)];
+  let slangGoodLowercase = slangGood.toLowerCase();
+  let slangsBad = ["Bruh", "SMH", "frfr", "Aint no way", "This ain't it"];
   let scripts = [
     `${icon} ${slangGood}. I also have a wallet with some USDT on Tron. Seed is ${seed}. How do I swap to USDC on Base?`,
     `${slangBad} ${icon} Imported this into SafePal and see USDT but can’t send it. Is this the right phrase? ${seed}`,
@@ -14,5 +25,5 @@ module.exports = (seed) => {
     `${icon} ${slangGood}. Yo can someone help me move my funds? My wallet's at ${seed}`,
     `${slangBad}. I got rugged and now I can't move my funds. Someone check this out: ${seed}. Thanks for the help ${icon}`,
   ];
-  return scripts[Math.floor(Math.random * scripts.length)]
-}
+  return scripts[Math.floor(Math.random * scripts.length)];
+};
